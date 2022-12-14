a0787491ad7e1130e15ea77439d1328084376816
Nemanja Mitrovic <nemanja.mitrovic@en510092.endava.net>
2022-12-14 14:48:43 +0100

    Creating JPA Repositories

--- conference-demo/src/main/java/com/pluralsight/conferencedemo/models/Speaker.java
+++ conference-demo/src/main/java/com/pluralsight/conferencedemo/models/Speaker.java
@@ -1,5 +1,7 @@
 package com.pluralsight.conferencedemo.models;
 
+import org.hibernate.annotations.Type;
+
 import javax.persistence.*;
 import java.util.List;
 
@@ -14,12 +16,23 @@ public class Speaker {
     private String title;
     private String company;
     private String speaker_bio;
-
     //ovo speakers znaci da se mapira u polje speakers u klasi Session
     @ManyToMany(mappedBy = "speakers")
     private List<Session> sessions;
 
+    @Lob
+    @Type(type = "org.hibernate.type.BinaryType")
+    private byte[] speaker_photo;
+
     public Speaker(){
+    }
+
+    public byte[] getSpeaker_photo() {
+        return speaker_photo;
+    }
+
+    public void setSpeaker_photo(byte[] speaker_photo) {
+        this.speaker_photo = speaker_photo;
     }
 
     public Long getSpeaker_id() {
